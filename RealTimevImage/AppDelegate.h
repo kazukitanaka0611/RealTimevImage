80697e5eed416e52d58ee036a0d1227d487ae86a
Kazuki Tanaka <zukeyny@gmail.com>
2012-06-29 20:48:48 +0900

    Initial Commit

---
+++ RealTimevImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RealTimevImage
+//
+//  Created by kazuki_tanaka on 12/06/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
